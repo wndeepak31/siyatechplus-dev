2b6f90c7b8e1d8bc137b3f10899a8914957b2e17
wndeepak31 <dnishad031@gmail.com>
2025-12-24 21:18:41 +0530

    feat: add sitemap.xml

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+
+export function GET() {
+  const baseUrl = 'https://siyatechplus.com'
+
+  const pages = [
+    '',
+    '/about',
+    '/services',
+    '/portfolio',
+    '/contact',
+  ]
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages
+  .map(
+    (path) => `
+  <url>
+    <loc>${baseUrl}${path}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${path === '' ? '1.0' : '0.8'}</priority>
+  </url>`
+  )
+  .join('')}
+</urlset>`
+
+  return new NextResponse(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
+}
